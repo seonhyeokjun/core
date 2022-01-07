310f4deacf99de3efce62c9d37dd5af2c8e951f1
seonhyeokjun <tjsrmatn@naver.com>
2022-01-08 01:59:17 +0900

    스프링 핵심 원리 이해2 - 객체 지향 원리 적용

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,10 +1,18 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MemberServiceTest {
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach
+    void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     void join(){
